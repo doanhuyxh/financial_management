a7432897ee5f45cca73bbeeda73cd0b8d811b097
doanhuyxh <doanhuyxh@gmail.com>
2026-09-21 14:16:29 +0700

    Update ESLint configuration to disable 'react-hooks/exhaustive-deps' rule.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = defineConfig([
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
+      "react-hooks/exhaustive-deps": "off",
     },
   }
 ]);
